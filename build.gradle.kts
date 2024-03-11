b1d7ff9fee136abd822df497b851cf6b782d7413
fennec <elijah17@gmail.com>
2024-03-11 18:07:25 +0900

    Update spring version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.2.2"
+    id("org.springframework.boot") version "3.2.3"
     id("io.spring.dependency-management") version "1.1.3"
     id("org.jmailen.kotlinter") version "3.14.0"
     id("com.avast.gradle.docker-compose") version "0.17.6"
